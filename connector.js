3c679371c8abb2e203c04fea7cbba36c9086b9a4
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2021-11-19 09:49:34 +0000

    update connector functions

--- connector.js
+++ connector.js
@@ -1,9 +1,8 @@
-const fetch = require("node-fetch")
-const recentProjects = require('./json/mostRecentProjects.json')
 const workflows = require('./json/workflows.json')
 const services = require('./json/services.json')
 const deployments = require('./json/deployments.json')
-var utils = require('./utils');
+const axios = require("axios")
+const utils = require('./utils');
 
 module.exports = class Connector {
   constructor() {
@@ -33,64 +32,97 @@ module.exports = class Connector {
     return utils.sort(deployments)
   }
 
-  async getDevfiles(token){
-
-    const headers = new fetch.Headers();
-    headers.append("Accept", "application/json");
-    headers.append("Authorization", "Bearer " + token)
-
-    const options = {
+  async getWorkspaces(token){
+    const config = {
       method: 'GET',
-      headers: headers,
-      redirect: 'follow'
-    };
-
-    const url = "https://che-smartclide-che.che.smartclide.eu/api/devfile?version=1.0.0"
-    console.log("\nFetch URL: " + url)
+      url: 'https://che-smartclide-che.che.smartclide.eu/api/workspace',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    console.log("Get workspaces list")
 
     try{
-      const res = await fetch(url, options)
-
-      if(res.status === 200){
-        const jsonData = await res.json()
-        return jsonData
-      }
-      else {
-        console.log("Error: " + res.status)
-      }
-      return null
+      const res = await axios(config)
+      return res.data
     } catch(e){
       console.log(e)
       throw e
     }
   }
 
-  async createDevfile(devfile, token){
+  async getWorkspacesWithType(token, type){
+    const ws = await this.getWorkspaces(token)
+    if(ws){
+      return ws.filter(a => a.devfile.attributes.type === type)
+    }
+    return []
+  }
 
-    const headers = new fetch.Headers();
-    headers.append("Accept", "text/html")
-    headers.append("Content-Type", "application/json")
-    headers.append("Authorization", "Bearer " + token)
+  async getRecentWorkspaces(token, limit = 5){
+    const workspaces = await this.getWorkspaces(token)
+    if(workspaces){
+      const sorted = utils.sortByUpdated(workspaces)
+      return sorted.slice(0, limit)
+    }
+    return []
+  }
 
-    const options = {
-      method: 'POST',
-      headers: headers,
-      redirect: 'follow',
-      body: JSON.stringify(devfile)
-    };
-    console.log("\nCreating devfile...")
+  async startWorkspace(token, workspaceId){
+    const url = `https://che-smartclide-che.che.smartclide.eu/api/workspace/${workspaceId}/runtime?debug-workspace-start=false`
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    console.log("Start workspace: " + workspaceId)
 
     try{
-      const res = await fetch(url, options)
+      const res = await axios.post(url, {}, config)
+      return JSON.stringify(res.data)
+    } catch(e){
+      console.log(e)
+      throw e
+    }
+  }
 
-      if(res.status === 200){
-        const jsonData = await res.json()
-        return jsonData
+  async stopWorkspace(token, workspaceId){
+    const config = {
+      method: 'delete',
+      url: `https://che-smartclide-che.che.smartclide.eu/api/workspace/${workspaceId}/runtime`,
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
       }
-      else {
-        console.log("Error: " + res.status)
+    }
+    console.log("Stop workspace: " + workspaceId)
+
+    try{
+      const res = await axios(config)
+      return JSON.stringify(res.data)
+    } catch(e){
+      console.log(e)
+      throw e
+    }
+  }
+
+  async getWorkspace(token, workspaceId){
+    const config = {
+      method: 'GET',
+      url: `https://che-smartclide-che.che.smartclide.eu/api/workspace/${workspaceId}?includeInternalServers=false`,
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
       }
-      return null
+    }
+    console.log("Details workspace: " + workspaceId)
+
+    try{
+      const res = await axios(config)
+      return res.data
     } catch(e){
       console.log(e)
       throw e
--- utils.js
+++ utils.js
@@ -3,6 +3,11 @@ module.exports = {
         return results.sort(function(a,b){
             return new Date(b.updatedAt) - new Date(a.updatedAt);
         });
+    },
+    sortByUpdated: function (results){
+        return results.sort(function(a,b){
+            return b.attributes.updated - a.attributes.updated;
+        });
     }
 
 }
